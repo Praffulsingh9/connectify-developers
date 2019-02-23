5c1724fd6755994f641d3a3e772e5d3d15f5c683
Praffulsingh9 <prafful.singh017@gmail.com>
2019-02-24 01:19:02 +0530

    Various dependencies and cdns configured

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
   }
 }
 
