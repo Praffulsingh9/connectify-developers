bfd3a31107552d3eed7fd41e2e38fb98f5e0f275
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-02 16:51:38 +0530

    React router being set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from "react";
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
 import Landing from "./components/Layout/Landing";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
