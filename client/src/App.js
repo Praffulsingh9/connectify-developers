fb86427bff32519b1b5fe79bc3a99bda9b08609f
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-02 20:03:21 +0530

    Redux was setup in the project

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,27 @@ import Footer from "./components/Layout/Footer";
 import Landing from "./components/Layout/Landing";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import store from "./store";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
