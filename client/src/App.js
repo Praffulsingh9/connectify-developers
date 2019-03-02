9bab1bd9cdbd2ab7a7e9028aca987a8cdb36082b
Praffulsingh9 <prafful.singh017@gmail.com>
2019-03-03 00:31:58 +0530

    login amd register functionality completed

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,27 @@ import Footer from "./components/Layout/Footer";
 import Landing from "./components/Layout/Landing";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
 import store from "./store";
 import "./App.css";
+
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+
+  store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now() / 1000;
+
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = "/login";
+  }
+}
 
 class App extends Component {
   render() {
